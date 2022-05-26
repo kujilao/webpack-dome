2e67ba53429017a81271c633579536456a71da75
kujilao <1754443646@qq.com>
2022-05-26 16:18:26 +0800

    部署测试

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,5 +12,42 @@ module.exports = {
             template: 'src/assets/index.html'
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: ['file-loader']
+                },
+            {
+                test: /\.styl$/,
+                use: [
+                 "style-loader",
+                  "css-loader",
+                  "stylus-loader"]
+                },
+            {
+                test: /\.less$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "less-loader",
+                ],
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('dart-sass'),
+                        }
+                    },
+                ],
+            }
+
+        ],
+    },
 }
 
