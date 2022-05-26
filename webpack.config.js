30696b444243d411fbe24449f92730396bfbfe4f
kujilao <1754443646@qq.com>
2022-05-26 14:51:57 +0800

    start和build配置完成

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,16 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
+
+const base = require('./webpack.config.base.js')
+
 module.exports = {
+    ...base,
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
     },
-    entry: './src/index.js',
-    output: {
-        filename: '[name].[contenthash].js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: '凉风',
-            template: 'src/assets/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // all options are optional
-            filename: "[name].[contenthash].css",
-            chunkFilename: "[id].[contenthash].css",
-            ignoreOrder: false, // Enable to remove warnings about conflicting order
-        }),
-    ],
     module: {
         rules: [
             {
